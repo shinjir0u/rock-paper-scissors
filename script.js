b6a11ea9b63123b886087c32f780a7ecaeb73914
Shinjirou <htooagwai9@gmail.com>
2024-10-20 21:14:52 +0630

    Add script.js with pseudo code

---
+++ script.js
@@ -0,0 +1,38 @@
+/*
+    A game of rock-paper-scissors with 5 rounds
+    Request user for input 
+    Randomly generate result and compare
+    Keep track of the results
+
+    Create variables for rock, paper, scissors
+    Set count variable and set it to 5
+    Create result variable
+
+    While count is greater than 0
+        Generate a random value (rock, paper, scissor)
+        Prompt a request to user for input
+        Validate the input
+        If valid
+            Compare the input with the generated value
+            Append result 'win','lose','tie'
+        Else 
+            Append result 'error'
+        End If
+        output the result
+        Minus one from count
+
+    End while
+
+    <-- Validate Function -->
+    Convert the answer to lower case 
+    Invalidate if the input isn't rock or paper or scissor
+
+    <-- Generate Function -->
+    Randomly generate a number (0, 1, 2)
+    Assign rock, paper, scissors with 0, 1, 2 respectively
+    Return the number
+
+    <-- Compare Function -->
+    
+*/
+
